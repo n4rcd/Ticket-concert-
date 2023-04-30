950c0b0a8a4d0a7148b30cb596f24dbf153fa2fa
KioFWO <margaux.picard@outlook.com>
2023-04-30 19:23:13 +0200

    MAJ Incroyable
    Tout a changé

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function changeImage() {
-  document.querySelector(".ticket").style.backgroundImage = "url(./1.jpeg)";
-}
-
-
-document.querySelector("#button1").addEventListener("click",changeImage);
